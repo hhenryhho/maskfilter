450e492b568be5683a694ed07e65327fbc2d0868
Henry Ho <henryho73@hotmail.com>
2022-02-01 14:25:23 -0500

    Defifrens

---
+++ components/Mask.js
@@ -0,0 +1,122 @@
+/** @format */
+
+import { Flex, Select } from '@chakra-ui/react'
+import React, { useRef, useEffect, useState } from 'react'
+import Webcam from 'react-webcam'
+import * as tf from '@tensorflow/tfjs'
+import * as facemesh from '@tensorflow-models/face-landmarks-detection'
+import { drawMesh, drawMask } from './utilities'
+
+const Mask = () => {
+	const webcamRef = useRef(null)
+	const canvasRef = useRef(null)
+	const [value, setValue] = useState('0')
+	// I'm dumb so I need another reference to check change in value state
+	const reference = useRef()
+	reference.current = value
+
+	//  Load posenet
+	const runFacemesh = async () => {
+		const net = await facemesh.load(
+			facemesh.SupportedPackages.mediapipeFacemesh
+		)
+		if (
+			typeof webcamRef.current !== 'undefined' &&
+			webcamRef.current !== null &&
+			webcamRef.current.video.readyState === 4
+		) {
+			// Get Video Properties
+			const videoWidth = webcamRef.current.video.videoWidth
+			const videoHeight = webcamRef.current.video.videoHeight
+
+			// Set video width
+			webcamRef.current.video.width = videoWidth
+			webcamRef.current.video.height = videoHeight
+
+			// Set canvas width
+			canvasRef.current.width = videoWidth
+			canvasRef.current.height = videoHeight
+		}
+
+		// Set loop to infinitely check
+		setInterval(() => {
+			detect(net)
+		}, 100)
+	}
+
+	const detect = async (net) => {
+		// Make Detections
+		const face = await net.estimateFaces({
+			input: webcamRef.current.video,
+		})
+
+		// Get canvas context
+		const ctx = canvasRef.current.getContext('2d')
+
+		// Clear anything currently on canvas
+		ctx.clearRect(
+			0,
+			0,
+			canvasRef.current.width,
+			canvasRef.current.height
+		)
+		// Rotate between masks
+		switch (reference.current) {
+			case '0':
+				drawMesh(face, ctx)
+				break
+			case '1':
+				drawMask(face, ctx)
+				break
+		}
+	}
+
+	return (
+		<Flex
+			h='100vh'
+			align='center'
+			justify='center'
+			flexDir='column'>
+			<Webcam
+				ref={webcamRef}
+				onLoadedData={runFacemesh}
+				style={{
+					position: 'absolute',
+					marginLeft: 'auto',
+					marginRight: 'auto',
+					left: 0,
+					right: 0,
+					textAlign: 'center',
+					zindex: 9,
+					width: 640,
+					height: 480,
+				}}
+			/>
+			<canvas
+				ref={canvasRef}
+				style={{
+					position: 'absolute',
+					marginLeft: 'auto',
+					marginRight: 'auto',
+					left: 0,
+					right: 0,
+					textAlign: 'center',
+					zindex: 9,
+					width: 640,
+					height: 480,
+				}}
+			/>
+			<Select
+				mt='600'
+				onChange={(e) => {
+					setValue(e.target.value)
+				}}
+				value={value}>
+				<option value='1'>Defi Frens</option>
+				<option value='0'>Mesh</option>
+			</Select>
+		</Flex>
+	)
+}
+
+export default Mask
---
+++ components/Utilities.js
@@ -0,0 +1,39 @@
+/** @format */
+
+// Drawing Mesh
+export const drawMask = (predictions, ctx) => {
+	const image = new Image()
+	// Load an image of intrinsic size 300x227 in CSS pixels
+	image.src = '/fren.png'
+
+	if (predictions.length > 0) {
+		// For each face
+		predictions.forEach((prediction) => {
+			const keypoints = prediction.scaledMesh
+			// Draw Dots
+			const x = keypoints[54][0]
+			const y = keypoints[54][1]
+
+			ctx.drawImage(image, x - 80, y - 130, 300, 300)
+		})
+	}
+}
+
+export const drawMesh = (predictions, ctx) => {
+	if (predictions.length > 0) {
+		predictions.forEach((prediction) => {
+			const keypoints = prediction.scaledMesh
+
+			// Draw Dots
+			for (let i = 0; i < keypoints.length; i++) {
+				const x = keypoints[i][0]
+				const y = keypoints[i][1]
+
+				ctx.beginPath()
+				ctx.arc(x, y, 1 /* radius */, 0, 3 * Math.PI)
+				ctx.fillStyle = 'aqua'
+				ctx.fill()
+			}
+		})
+	}
+}
